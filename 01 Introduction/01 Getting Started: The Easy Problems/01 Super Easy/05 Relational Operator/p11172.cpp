6c91562015e7a97ed27b9dc9999dd4b7675dfa9e
Gilang Gumilar <Gilang Gumilar>
2018-01-06 22:06:01 +0700

    Add 11172 - Relational Operator

---
+++ 01 Introduction/01 Getting Started: The Easy Problems/01 Super Easy/05 Relational Operator/p11172.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int a;
+    int b;
+
+    cin >> a >> b;
+
+    if (a == b) {
+      cout << "=" << endl;
+    } else if (a > b) {
+      cout << ">" << endl;
+    } else if (a < b) {
+      cout << "<" << endl;
+    } else {
+      cout << "?" << endl;
+    }
+
+  }
+  return 0;
+}
